bd8e139c2eaf68395190e5dbac4272fa95d949d5
jedikokuma <jedi.kokuma@gmail.com>
2026-08-20 16:37:10 +0100

    changes made to primetask

--- jedi.js
+++ jedi.js
@@ -1,10 +1,7 @@
 const isPrime = (num) => {
-  for (let i = 59; i < num; i++) {
-    if (num % i === 0) {
-      return false;
+    if (num <= 1) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) return false;
     }
-  }
-  return num > 1;
-};
-
-console.log(isPrime(7));
+    return true;
+}
