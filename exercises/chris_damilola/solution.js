c7c27d8d84d6e797e6f495a2a452c29638a751d3
chris-damilola <damilolaogunleye420@gmail.com>
2026-08-20 12:59:31 +0100

    added challenge's solution

--- exercises/chris_damilola/solution.js
+++ exercises/chris_damilola/solution.js
@@ -3,3 +3,10 @@ const reversedString = (name) => {
   return name.split("").reverse().join("");
 };
 console.log(reversedString("chris"));
+
+//  Write an arrow function called `isPrime` that takes a number as input. It should return `true` if the number is prime, and `false` otherwise.
+const isPrime = (num) => {
+  if (num % 2 === 0) return "true";
+  else return "false";
+};
+console.log(isPrime(25));
